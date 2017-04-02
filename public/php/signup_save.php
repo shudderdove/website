ecdba9eec1a31b18f94ba830e1112b366af4f0ad
wnolan <wnolan1@student.gsu.edu>
2017-04-02 06:54:27 -0400

    Moved images into img folder

---
+++ public/php/signup_save.php
@@ -0,0 +1,26 @@
+<?php
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$email = $_POST['email'];
+	$city = $_POST['city'];
+	$state = $_POST['state'];
+	$about = $_POST['about'];
+
+	echo "<p>$fname</p>";
+	echo "<p>$lname</p>";
+	echo "<p>$email</p>";
+	echo "<p>$city</p>";
+	echo "<p>$state</p>";
+	echo "<p>$about</p>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>php</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+
+</body>
+</html>
